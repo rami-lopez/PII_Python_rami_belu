d693548391b4596d98f98a97e04de1e8dd17580e
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2024-08-17 12:00:23 -0300

    Solucion ejercicio Simetrico

--- CSharp/5_SymDiff/Program.cs
+++ CSharp/5_SymDiff/Program.cs
@@ -2,15 +2,33 @@
 
 ArrayList list1 = new ArrayList { 1, 2, 3 };
 ArrayList list2 = new ArrayList { 3, 4, 5 };
+ArrayList list3 = new ArrayList { };
 
-Console.WriteLine($"[{string.Join(",", Functions.SymmetricDifference(list1, list2))}]");
+Functions.SymmetricDifference(list1, list2, list3);
+
+foreach (int element in list3)
+{
+    Console.WriteLine(element);
+}
 
 public static class Functions
 {
-    public static ArrayList SymmetricDifference(ArrayList list1, ArrayList list2)
+    public static void SymmetricDifference(ArrayList list1, ArrayList list2, ArrayList list3)
     {
-        // Reemplaza esto 👇por tu código 
-        return new ArrayList() { 1, 2, 4, 5 };
+        foreach (int i in list1)
+        {
+            if (list2.Contains(i) == false)
+            {
+                list3.Add(i);
+            }
+        }
+
+        foreach (int i in list2)
+        {
+            if (list1.Contains(i) == false)
+            {
+                list3.Add(i);
+            }
+        }
     }
 }
-
